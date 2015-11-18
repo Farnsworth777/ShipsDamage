6fe2e2f6f3b976ceb64f22b6e11a647776cf112c
Alec <zlitz93@gmail.com>
2015-11-18 11:05:41 -0500

    Base Code

---
+++ src/ShipsDamage/ShipsDamage.java
@@ -0,0 +1,71 @@
+package ShipsDamage;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import MoseShipsBukkit.Events.ShipMovingEvent;
+import MoseShipsBukkit.StillShip.Vessel.MovableVessel;
+import MoseShipsBukkit.StillShip.Vessel.Vessel;
+
+public class ShipsDamage extends JavaPlugin implements Listener{
+	
+	JavaPlugin PLUGIN;
+	
+	public static final File CONFIG = new File("plugins/ShipsDamage/Config.yml");
+	
+	public void onEnable(){
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+	@EventHandler
+	public void onShipMove(ShipMovingEvent event){
+		MovableVessel vessel = event.getVessel();
+		int shipssize = vessel.getStructure().getAllBlocks().size();
+	}
+	public static void writeToVesselDB(Vessel vessel, String location, Object write){
+		File file = vessel.getFile();
+		YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+		config.set(location, write);
+		try {
+			config.save(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrawce();
+		}
+	}
+	
+	public static String getStringFromVesselDB(Vessel vessel, String location){
+		File file = vessel.getFile();
+		YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+		String ret = config.getString(location);
+		return ret;
+	}
+	
+	public static void writeToConfig(String location, Object write){
+		YamlConfiguration config = YamlConfiguration.loadConfiguration(CONFIG);
+		config.set(location, write);
+		try {
+			config.save(CONFIG);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static String getString(String location){
+		YamlConfiguration config = YamlConfiguration.loadConfiguration(CONFIG);
+		String ret = config.getString(location);
+		return ret;
+	}
+
+}
--- src/ShipsDamage/Test.java
+++
@@ -1,5 +0,0 @@
-package ShipsDamage;
-
-public class Test {
-
-}
